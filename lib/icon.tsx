7c611eccfc6ddbbb7faab5b85e8e976c48acfc1d
Bryant-Wang24 <485969746@qq.com>
2021-12-30 21:27:34 +0800

    添加css,配置css相关的loader

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './importIcons'
+import './icon.scss';
 
 interface IconProps{
   name:string
 }
 const Icon:React.FC<IconProps> = (props)=>{
   return(
-    <span>
-      <svg>
+      <svg className="fui-icon">
         <use xlinkHref={`#${props.name}`}/>
       </svg>
-    </span>
   )
 }
 export default Icon
