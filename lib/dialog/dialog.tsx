2d6b3b7f09ae6b998952ba4e518cc477f083bbf4
Bryant-Wang24 <485969746@qq.com>
2022-01-11 09:24:15 +0800

    重构 dialog 的 API

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,6 +1,6 @@
 import { scopedClassMaker } from '../classes';
 import './dialog.scss'
-import React, {Fragment, ReactElement, ReactFragment, ReactNode} from 'react';
+import React, {Fragment, ReactElement, ReactNode} from 'react';
 import {Icon} from '../index';
 import ReactDOM from 'react-dom';
 interface Props{
@@ -22,7 +22,7 @@ const Dialog:React.FC<Props> = (props)=>{
       props.onClose(e)
     }
   }
-  const x = props.visible ?
+  const result = props.visible &&
     <Fragment>
       <div className={sc('mask')} onClick={onClickMask}>
       </div>
@@ -43,66 +43,51 @@ const Dialog:React.FC<Props> = (props)=>{
         </footer>
       </div>
     </Fragment>
-    :
-    null;
   return (
-    ReactDOM.createPortal(x, document.body)
+    ReactDOM.createPortal(result, document.body)
   );
 }
 Dialog.defaultProps = {
   closeOnClickMask:false
 }
-const alert = (content:string)=>{
+
+const modal = (content:ReactNode,buttons?:Array<ReactElement>,afterClose?: () => void)=>{
   const close = ()=>{
     ReactDOM.render(React.cloneElement(component,{visible:false}),div)//隐藏掉这个组件
     ReactDOM.unmountComponentAtNode(div)//卸载被隐藏的组件
     div.remove()//删除组件
   }
-  const component = <Dialog visible={true} buttons={[<button onClick={close}>yes</button>]} onClose={()=>{
-    ReactDOM.render(React.cloneElement(component,{visible:false}),div)
-    ReactDOM.unmountComponentAtNode(div)
+  const component = <Dialog visible={true} buttons={buttons} onClose={()=>{
+   close()
+    afterClose&&afterClose()
   }}>{content}</Dialog>
   const div = document.createElement('div')
   document.body.append(div)
   ReactDOM.render(component,div)
+  return close
+}
+
+const alert = (content:string)=>{
+ const button = <button onClick={()=>close()}>OK</button>
+  const close = modal(content,[button])
 }
 const confirm = (content:string,yes?:()=>void,no?:()=>void)=>{
+
   const onYes = ()=>{
-    ReactDOM.render(React.cloneElement(component,{visible:false}),div)//隐藏掉这个组件
-    ReactDOM.unmountComponentAtNode(div)//卸载被隐藏的组件
-    div.remove()//删除组件
+    close()
     yes&&yes()
   }
   const onNo = ()=>{
-    ReactDOM.render(React.cloneElement(component,{visible:false}),div)//隐藏掉这个组件
-    ReactDOM.unmountComponentAtNode(div)//卸载被隐藏的组件
-    div.remove()//删除组件
+    close()
     no&&no()
   }
-  const component = <Dialog visible={true} onClose={onNo} buttons={[
-      <button onClick={onYes}>yes</button>,
-      <button onClick={onNo}>no</button>
-  ]}>{content}</Dialog>
-  const div = document.createElement('div')
-  document.body.append(div)
-  ReactDOM.render(component,div)
+  const buttons =[
+    <button onClick={onYes}>yes</button>,
+    <button onClick={onNo}>no</button>
+  ]
+  const close = modal(content,buttons,no)
 }
 
-const modal = (content:ReactNode |ReactFragment,buttons?:Array<ReactElement>,afterClose?:()=>void)=>{
-  const close = ()=>{
-    ReactDOM.render(React.cloneElement(component,{visible:false}),div)//隐藏掉这个组件
-    ReactDOM.unmountComponentAtNode(div)//卸载被隐藏的组件
-    div.remove()//删除组件
-  }
-  const component = <Dialog visible={true} onClose={()=>{
-    close()
-    afterClose&&afterClose()
-  }} >{content}</Dialog>
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  ReactDOM.render(component,div)
-  return close;
-}
 export {alert,confirm,modal}
 
 export default Dialog
