01f39abfb5e9cc0e83f241eca10f14cd852e0048
Bryant-Wang24 <485969746@qq.com>
2022-01-13 10:38:35 +0800

    判断Layout组件里面有没有Aside,添加样式

--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -9,12 +9,41 @@ const x: React.FC = () => {
     return (
         <div>
             <h1>第四个例子</h1>
-            <Layout style={{height:500}} className='layout'>
-                <Aside>aside</Aside>
-                <Header>header</Header>
-                <Content>content</Content>
-                <Footer>footer</Footer>
-            </Layout>
+            <main>
+            <section>
+                <h2>例子1</h2>
+                <Layout style={{height:500}} className='layout'>
+                    <Header>header</Header>
+                    <Content>content</Content>
+                    <Footer>footer</Footer>
+                </Layout>
+            </section>
+
+
+            <section>
+                <h2>例子2</h2>
+                <Layout style={{height:500}} className='layout'>
+                    <Header>header</Header>
+                    <Layout style={{height:500}}>
+                        <Aside>aside</Aside>
+                        <Content>content</Content>
+                    </Layout>
+                    <Footer>footer</Footer>
+                </Layout>
+            </section>
+
+                <section>
+                    <h2>例子3</h2>
+                    <Layout style={{height:500}} className='layout'>
+                        <Aside>aside</Aside>
+                        <Layout>
+                            <Header>header</Header>
+                            <Content>content</Content>
+                            <Footer>footer</Footer>
+                        </Layout>
+                    </Layout>
+                </section>
+            </main>
         </div>
     )
 }
--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, {ReactElement} from 'react'
 import {scopedClassMaker} from "../classes";
 import './layout.scss'
+import Aside from "./aside";
 
 const  sc = scopedClassMaker('gu-layout')
 
 interface Props extends React.HTMLAttributes<HTMLElement>{
-
+    children:ReactElement|Array<ReactElement>
 }
 const Layout: React.FC<Props> = (props) => {
     const {className,...rest} = props
+    const children = props.children as Array<ReactElement>
+    const hasAside = children.length && children.some((item)=>{
+        return item.type===Aside
+    })
     return (
-        <div className={[sc('',{extra:className})].join(' ')} {...rest}>
+        <div className={sc('',{extra:[className,hasAside&&'hasAside'].join(' ')})} {...rest}>
             {props.children}
         </div>
     )
