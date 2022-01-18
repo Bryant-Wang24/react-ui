d1992f2d5b99c623404008c79dd073c63eee8a76
Bryant-Wang24 <485969746@qq.com>
2022-01-18 08:57:44 +0800

    重构 scopedClassMaker

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -12,19 +12,28 @@ interface ClassToggles {
 }
 function scopedClassMaker(prefix: string) {
 
-    return function x(name?: string | ClassToggles,options?:Options) {
+    return function x(name: string | ClassToggles,options?:Options) {
         //name = {hasAside:true,active:false,x:true,y:false}
-        let name2;
-        let result
-        if (typeof name==='string'||name ===undefined){
-            name2 = name
-            result = [prefix,name2].filter(Boolean).join('-') //如果传进来字符串 'header'，直接合并就变成 gu-layout-header
-        }else{
-            name2 = Object.entries(name).filter(kv=>kv[1]).map(kv=>kv[0]) //如果传进来的是对象，先过滤掉 value为false 的 key，比如 {hasAside:true,active:false,x:true,y:false} 过滤后就变成 ['hasAside','x']
-            result = name2.map(n=>{
-                return [prefix,n].filter(Boolean).join('-') // 合并后变成 ['.gu-layout-hasAside','.gu-layout-x']
-            }).join(' ') //数组变成字符串 '.gu-layout-hasAside .gu-layout-x'
-        }
+        const namesObject = (typeof name==='string'||name===undefined)? {[name]:name} :name
+
+        const result = Object
+            .entries(namesObject)
+            .filter(kv=>kv[1]!==false)
+            .map(kv=>kv[0])
+            .map(name=>[prefix,name]
+                .filter(Boolean)
+                .join('-'))
+            .join(' ')
+        // if (typeof name==='string'||name ===undefined){
+        //     name2 = name
+        //     result = [prefix,name2].filter(Boolean).join('-') //如果传进来字符串 'header'，直接合并就变成 gu-layout-header
+        // }else{
+        //     name2 = Object.entries(name).filter(kv=>kv[1]).map(kv=>kv[0]) //如果传进来的是对象，先过滤掉 value为false 的 key，比如 {hasAside:true,active:false,x:true,y:false} 过滤后就变成 ['hasAside','x']
+        //     result = name2.map(n=>{
+        //         return [prefix,n].filter(Boolean).join('-') // 合并后变成 ['.gu-layout-hasAside','.gu-layout-x']
+        //     }).join(' ') //数组变成字符串 '.gu-layout-hasAside .gu-layout-x'
+        // }
+
         if (options&&options.extra){
             return [result,options&&options.extra].filter(Boolean).join(' ')
         }else{
