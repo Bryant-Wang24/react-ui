d6c666e0789c782a0d1266524027998af7962b74
Bryant-Wang24 <485969746@qq.com>
2022-07-06 23:27:59 +0800

    button支持size和level

--- lib/button.example.tsx
+++ lib/button.example.tsx
@@ -7,13 +7,54 @@ const ButtonExample: React.FunctionComponent = () => {
       <h1>Button示例</h1>
       <section>
         <h2>常规使用</h2>
-          <main className='normal'>
-              <Button type='default'>123</Button>
-              <Button type='primary'>按钮</Button>
+          <main className='default-main'>
+              <Button>常规按钮</Button>
+              <Button theme='link'>链接按钮</Button>
+              <Button theme='text'>文本按钮</Button>
           </main>
-
       </section>
 
+        <section>
+            <h2>支持size</h2>
+            <main className='size-main'>
+                <div className='size-content'>
+                    <Button size='big'>大大按钮</Button>
+                    <Button>普通按钮</Button>
+                    <Button size='small'>小小按钮</Button>
+                </div>
+                <div className='size-content'>
+                    <Button theme='link' size='big'>大大按钮</Button>
+                    <Button theme='link'>普通按钮</Button>
+                    <Button theme='link' size='small'>小小按钮</Button>
+                </div>
+                <div className='size-content'>
+                    <Button theme='text' size='big'>大大按钮</Button>
+                    <Button theme='text'>普通按钮</Button>
+                    <Button theme='text' size='small'>小小按钮</Button>
+                </div>
+            </main>
+        </section>
+
+        <section>
+            <h2>支持level</h2>
+            <main className='main'>
+                <div className='content'>
+                    <Button level='main'>主要按钮</Button>
+                    <Button>普通按钮</Button>
+                    <Button level='danger'>危险按钮</Button>
+                </div>
+                <div className='content'>
+                    <Button theme='link' level='main'>主要链接按钮</Button>
+                    <Button theme='link'>普通链接按钮</Button>
+                    <Button theme='link' level='danger'>危险链接按钮</Button>
+                </div>
+                <div className='content'>
+                    <Button theme='text' level='main'>主要文字按钮</Button>
+                    <Button theme='text'>普通文字按钮</Button>
+                    <Button theme='text' level='danger'>危险文字按钮</Button>
+                </div>
+            </main>
+        </section>
     </div>
   );
 };
--- lib/button.tsx
+++ lib/button.tsx
@@ -2,14 +2,16 @@ import React from "react"
 import classes from "./helpers/classes";
 import './button.scss'
 interface ButtonProps{
-    type?:string,
+    theme?:string | 'normal'
+    size?:string,
+    level?:string
 }
 const Button:React.FC<ButtonProps> = (props) =>{
-    const {type } = props
+    const {theme,size,level } = props
   return (
-    <span className={classes('fui-button', `gulu-button-theme-${type}`) }>
+    <button className={classes('fui-button', `gulu-button-theme-${theme}`,`gulu-button-size-${size}`,`gulu-button-level-${level}`) }>
         {props.children}
-    </span>
+    </button>
   )
 }
 
