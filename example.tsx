dccfeae8629f2ce2ec972146f52ca23e09b252fe
Bryant-Wang24 <485969746@qq.com>
2022-07-07 22:47:12 +0800

    raw-loader展示文件代码

--- example.tsx
+++ example.tsx
@@ -3,12 +3,15 @@ import './example.scss'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter as Router, Route, NavLink} from 'react-router-dom';
-import IconExample from './lib/icon/icon.example';
 import ButtonExample from './lib/button.example';
 import DialogExample from './lib/dialog/dialog.example';
 import LayoutExample from './lib/layout/layout.example'
 import {Layout, Aside, Header, Content, Footer} from './lib/layout/layout';
 import Icon from "./lib/icon/icon";
+import IconDemo from "./lib/icon/icon.demo";
+
+const x = require('!!raw-loader!./lib/icon/icon.example')
+console.log(x.default)
 
 ReactDOM.render(
   <Router>
@@ -38,7 +41,7 @@ ReactDOM.render(
           </ul>
         </Aside>
         <Content className='docContent'>
-          <Route path="/icon" component={IconExample}/>
+          <Route path="/icon" component={IconDemo}/>
           <Route path="/button" component={ButtonExample}/>
           <Route path="/dialog" component={DialogExample}/>
           <Route path="/layout" component={LayoutExample}/>
