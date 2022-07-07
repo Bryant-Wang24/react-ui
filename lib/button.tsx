29cb92e6b0ab05dc6396fb7f0ee3e4fc0a871800
Bryant-Wang24 <485969746@qq.com>
2022-07-07 23:34:29 +0800

    prism-react-renderer 高亮展示源代码

--- lib/button.tsx
+++ lib/button.tsx
@@ -1,15 +1,17 @@
 import React from "react"
 import classes from "./helpers/classes";
 import './button.scss'
-interface ButtonProps{
+interface ButtonProps extends React.HTMLAttributes<HTMLElement>{
     theme?:string | 'normal'
     size?:string,
     level?:string,
     disabled?:boolean,
     loading?:boolean,
+    // onClick?:React.MouseEventHandler<HTMLButtonElement>,
 }
 const Button:React.FC<ButtonProps> = (props) =>{
-    const {theme,size,level,disabled,loading } = props
+    const {theme,size,level,disabled,loading,...rest } = props
+    console.log(rest)
   return (
     <button className={
         classes('fui-button',
@@ -19,7 +21,7 @@ const Button:React.FC<ButtonProps> = (props) =>{
             `gulu-button-disabled-${disabled}`,
             `gulu-button-loading-${loading}`
         )
-    }>
+    } {...rest}>
         {loading && <span className="gulu-loadingIndicator"/>}
         {props.children}
     </button>
