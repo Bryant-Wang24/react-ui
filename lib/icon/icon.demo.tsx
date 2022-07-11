5b0c3382db68958caaa93eb890c38a5907fef383
Bryant-Wang24 <485969746@qq.com>
2022-07-11 14:55:56 +0800

    :art: change icon style

--- lib/icon/icon.demo.tsx
+++ lib/icon/icon.demo.tsx
@@ -4,12 +4,17 @@ import Demo from '../../demo';
 
 const IconDemo = () => {
     return (
-        <Demo code={require('!!raw-loader!./icon.example.tsx').default}>
-            <IconExample/>
-            <div className='explain'>
-                通过 name 属性来指定需要使用的图标
-            </div>
-        </Demo>
+        <div className='icon-example'>
+            <h1>Icon示例</h1>
+            <Demo code={require('!!raw-loader!./icon.example.tsx').default}>
+                <main>
+                    <IconExample/>
+                </main>
+                <div className='explain'>
+                    通过 name 属性来指定需要使用的图标
+                </div>
+            </Demo>
+        </div>
     );
 };
 
